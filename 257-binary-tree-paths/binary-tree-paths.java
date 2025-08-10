9d39ab596fc27972170ca9be9be84add772c4b90
Batool samen <116743695+batool-samen@users.noreply.github.com>
2025-08-10 23:00:36 +0300

    Time: 7 ms (65.94%) | Memory: 42.4 MB (93.20%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) return paths;
+        dfs(root, "", paths);
+        return paths;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> paths) {
+        if (node == null) return;
+
+        if (path.isEmpty()) {
+            path = Integer.toString(node.val);
+        } else {
+            path += "->" + node.val;
+        }
+
+        if (node.left == null && node.right == null) {
+            paths.add(path);
+            return;
+        }
+
+        dfs(node.left, path, paths);
+        dfs(node.right, path, paths);
+    }
+}
