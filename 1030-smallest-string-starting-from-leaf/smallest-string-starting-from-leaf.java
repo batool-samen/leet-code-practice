c8be1fdbd67edd7c795969b44226427ffd904dbc
Batool samen <116743695+batool-samen@users.noreply.github.com>
2025-09-01 00:13:36 +0300

    Time: 2 ms (86.71%) | Memory: 44.7 MB (72.23%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private String smallest = "~"; 
+
+    public String smallestFromLeaf(TreeNode root) {
+        dfs(root, new StringBuilder());
+        return smallest;
+    }
+
+    private void dfs(TreeNode node, StringBuilder path) {
+        if (node == null) return;
+
+        path.append((char) ('a' + node.val));
+
+        if (node.left == null && node.right == null) {
+            String candidate = path.reverse().toString(); 
+            path.reverse();
+            if (candidate.compareTo(smallest) < 0) {
+                smallest = candidate;
+            }
+        }
+
+        dfs(node.left, path);
+        dfs(node.right, path);
+
+        path.deleteCharAt(path.length() - 1);
+    }
+}
